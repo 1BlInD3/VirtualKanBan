be01010ca5f160070f6b8857fa031b7a602a4acf
Attila Bálind <attila.balind.repo@gmail.com>
2021-03-19 09:32:52 +0100

    tabla javitas

--- app/src/main/java/com/fusetech/virtualkanban/Fragments/PolcLocationFragment.kt
+++ app/src/main/java/com/fusetech/virtualkanban/Fragments/PolcLocationFragment.kt
@@ -25,13 +25,17 @@ class PolcLocationFragment : Fragment(), PolcLocationAdapter.PolcItemClickListen
 
         val frameLayout = view.myFrameLayout
         val child = layoutInflater.inflate(R.layout.polc_location_header,null)
-       // frameLayout.addView(child)
+        frameLayout.addView(child)
 
         recyclerView = view.polcRecycler
         myItems.clear()
        // loadData()
         myItems.add(PolcLocation("H220","20"))
         myItems.add(PolcLocation("H220","21"))
+        myItems.add(PolcLocation("H220","22"))
+        myItems.add(PolcLocation("H220","23"))
+        myItems.add(PolcLocation("H220","24"))
+        myItems.add(PolcLocation("H220","25"))
         recyclerView.adapter = PolcLocationAdapter(myItems, this)
         recyclerView.layoutManager = LinearLayoutManager(view.context)
         recyclerView.setHasFixedSize(true)
