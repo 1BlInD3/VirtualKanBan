28317df44e634edded58ca36609d821b4037836b
Attila Bálind <attila.balind.repo@gmail.com>
2021-04-08 13:08:39 +0200

    3as4es6os layout recycler pozicio

--- app/src/main/java/com/fusetech/virtualkanban/Fragments/IgenyKontenerLezarasFragment.kt
+++ app/src/main/java/com/fusetech/virtualkanban/Fragments/IgenyKontenerLezarasFragment.kt
@@ -5,20 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import com.fusetech.virtualkanban.R
+import kotlinx.android.synthetic.main.fragment_igeny_kontener_lezaras.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [IgenyKontenerLezarasFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class IgenyKontenerLezarasFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+    private lateinit var dataFrame: FrameLayout
     private var param1: String? = null
     private var param2: String? = null
 
@@ -34,8 +31,12 @@ class IgenyKontenerLezarasFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_igeny_kontener_lezaras, container, false)
+        val view = inflater.inflate(R.layout.fragment_igeny_kontener_lezaras, container, false)
+        dataFrame = view.data_frame1
+        val child = layoutInflater.inflate(R.layout.konteneres_view,null)
+        dataFrame.addView(child)
+
+        return view
     }
 
     companion object {
