b1d8fd37065ffa8692ad6b1fd15979c0bace3140
Attila Bálind <attila.balind.repo@gmail.com>
2021-04-16 14:29:19 +0200

    mielott egysegesitenem a fragmenteket

--- app/src/main/java/com/fusetech/virtualkanban/Fragments/IgenyKontenerKiszedesFragment.kt
+++ app/src/main/java/com/fusetech/virtualkanban/Fragments/IgenyKontenerKiszedesFragment.kt
@@ -1,5 +1,6 @@
 package com.fusetech.virtualkanban.Fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -17,24 +18,29 @@ import com.fusetech.virtualkanban.Adapters.KontenerAdapter
 import com.fusetech.virtualkanban.DataItems.KontenerItem
 import com.fusetech.virtualkanban.R
 import kotlinx.android.synthetic.main.fragment_igeny_kontener_kiszedes.view.*
-import kotlinx.android.synthetic.main.fragment_load.*
 import kotlinx.android.synthetic.main.konteneres_view.view.*
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-private lateinit var childFrame: FrameLayout
-private lateinit var childRecycler: RecyclerView
-private var kontenerList: ArrayList<KontenerItem> = ArrayList()
-private lateinit var progress: ProgressBar
-private lateinit var megnyitottBtn : Button
-private lateinit var exit3Btn: Button
-private lateinit var mainActivity: MainActivity
-private const val TAG = "IgenyKontenerKiszedesFr"
+
 
 class IgenyKontenerKiszedesFragment : Fragment(),KontenerAdapter.onKontenerClickListener {
 
+    private lateinit var childFrame: FrameLayout
+    private lateinit var childRecycler: RecyclerView
+    private var kontenerList: ArrayList<KontenerItem> = ArrayList()
+    private lateinit var progress: ProgressBar
+    private lateinit var megnyitottBtn : Button
+    private lateinit var exit3Btn: Button
+    private lateinit var mainActivity: MainActivity
+    private val TAG = "IgenyKontenerKiszedesFr"
+    private lateinit var kontenerKiszedes: sendKiszedesContainer
     private var param1: String? = null
     private var param2: String? = null
+
+    interface sendKiszedesContainer{
+        fun sendContainerKiszedes(kontener: String)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -80,20 +86,11 @@ class IgenyKontenerKiszedesFragment : Fragment(),KontenerAdapter.onKontenerClick
        return view
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            IgenyKontenerKiszedesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-
     override fun onKontenerClick(position: Int) {
         Toast.makeText(view?.context, "itt mas jelenik meg", Toast.LENGTH_SHORT).show()
+        kontenerKiszedes.sendContainerKiszedes(kontenerList[position].kontner_id.toString())
     }
+    @Suppress("UNCHECKED_CAST")
     private fun loadData(){
         try {
             kontenerList.clear()
@@ -110,5 +107,14 @@ class IgenyKontenerKiszedesFragment : Fragment(),KontenerAdapter.onKontenerClick
     }
     fun setProgressBarOn(){
         progress.visibility = View.VISIBLE
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        kontenerKiszedes = if (context is sendKiszedesContainer){
+            context
+        }else{
+            throw RuntimeException(context.toString() + "must implement")
+        }
     }
 }
