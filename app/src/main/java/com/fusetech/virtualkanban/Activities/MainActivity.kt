b3411d4346e1aa379ae0c8d6d21e2ba1faf2d737
Attila Bálind <attila.balind.repo@gmail.com>
2021-09-09 10:14:38 +0200

    navbar eltüntetve

--- app/src/main/java/com/fusetech/virtualkanban/Activities/MainActivity.kt
+++ app/src/main/java/com/fusetech/virtualkanban/Activities/MainActivity.kt
@@ -512,7 +512,7 @@ class MainActivity : AppCompatActivity(),
             }
         }
         this.registerReceiver(logoutWhenCharging, IntentFilter(mIntent))
-
+        hideSystemUI()
     }
 
     private fun cancelTimer() {
@@ -1473,5 +1473,16 @@ class MainActivity : AppCompatActivity(),
         }catch (e: java.lang.Exception){
             return false
         }
+    }
+    private fun hideSystemUI() {
+        // Set the IMMERSIVE flag.
+        // Set the content to appear under the system bars so that the content
+        // doesn't resize when the system bars hide and show.
+        this.window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+               // or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
     }
 }
